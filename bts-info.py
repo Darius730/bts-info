ef5a431b7e133e16215fd343bacce56eb4bb8de4
Darius <68792754+Darius730@users.noreply.github.com>
2020-08-01 23:04:33 +0300

    Add files via upload

---
+++ bts-info.py
@@ -0,0 +1,53 @@
+bts = {
+    "rm" : "Stage Name: RM (아르엠), formerly Rap Monster (랩몬스터)"
+"Full Name: Kim Nam Joon (김남준)"
+"Position: Leader, Main Rapper"
+"Birthday: September 12, 1994"
+"Zodiac Sign: Virgo"
+"Height: 181 cm (5’11”)"
+"Weight: 74 kg (162 lbs)",
+
+    "jin" : "Stage Name: Jin (진)"
+"Full Name: Kim Seok Jin (김석진)"
+"Position: Sub Vocalist, Visual"
+"Birthday: December 4, 1992"
+"Zodiac Sign: Sagittarius"
+"Height: 179 cm (5’10” 1/2)"
+"Weight: 63 kg (139 lbs)",
+    "suga" : "Stage Name: Suga (슈가)"
+"Full Name: Min Yoon Gi (민윤기)"
+"Position: Lead Rapper"
+"Birthday: March 9, 1993"
+"Zodiac Sign: Pisces"
+"Height: 173.8 cm (5’8.5″)"
+"Weight: 59 kg (130 lbs)",
+    "jhope" : "Stage Name: J-Hope (제이홉)"
+"Full Name: Jung Ho Seok (정호석)"
+"Position: Main Dancer, Sub Rapper, Sub Vocalist"
+"Birthday: February 18, 1994"
+"Zodiac Sign: Aquarius"
+"Height: 177 cm (5’10”)"
+"Weight: 65 kg (143 lbs)",
+    "jimin" : "Stage Name: Jimin (지민)"
+"Full Name: Park Ji Min (박지민)"
+"Position: Main Dancer, Lead Vocalist"
+"Birthday: October 13, 1995"
+"Zodiac Sign: Libra"
+"Height: 173.6 cm (5’8.3″)"
+"Weight: 60 kg (132 lbs)",
+    "v" : "Stage Name: V (뷔)"
+"Full Name: Kim Tae Hyung (김태형)"
+"Position: Lead Dancer, Sub Vocalist, Visual"
+"Birthday: December 30, 1995"
+"Zodiac Sign: Capricorn"
+"Height: 179 cm (5’10″)"
+"Weight: 63 kg (139 lbs)",
+    "jungkook" : "Stage Name: Jungkook (정국)"
+"Full Name: Jeon Jung Kook (전정국)"
+"Position: Main Vocalist, Lead Dancer, Sub Rapper, Center, Maknae"
+"Birthday: September 1, 1997"
+"Zodiac Sign: Virgo"
+"Height: 178 cm (5’10”)"
+"Weight: 66 kg (145 lbs)"
+}
+print(bts)
